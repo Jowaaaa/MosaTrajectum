5565e77d6432b63970683a2d41832efb88f42a6b
Jowaaaa <jobvdw.jvdw@gmail.com>
2023-06-19 15:04:52 +0200

    initial spaget commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var randNum = Math.round(Math.random()*100)
+  var randNum2 = Math.round(Math.random()*100)
+  var randNum3 = Math.round(Math.random()*100)
+
+  
+
+  const bins = [ //verschillende ranges 
+    {
+      status: 'rustig',
+      predicate: randNum => randNum < 50,
+    },
+    {
+      status: 'middel',
+      predicate: randNum => randNum < 75,
+    },
+    {
+      status: 'druk',
+      predicate: randNum => randNum > 75,
+    }
+  ]
+
+  const bins2 = [ //verschillende ranges 
+    {
+      status: 'rustig',
+      predicate: randNum => randNum < 50,
+    },
+    {
+      status: 'middel',
+      predicate: randNum => randNum < 75,
+    },
+    {
+      status: 'druk',
+      predicate: randNum => randNum > 75,
+    }
+  ]
+  const bins3 = [ //verschillende ranges 
+    {
+      status: 'rustig',
+      predicate: randNum => randNum < 50,
+    },
+    {
+      status: 'middel',
+      predicate: randNum => randNum < 75,
+    },
+    {
+      status: 'druk',
+      predicate: randNum => randNum > 75,
+    }
+  ]
+
+  const parkingStatus = bins.find(bin => bin.predicate(randNum)).status; //zoek eerste bin die true teruggeeft.
+  const parkingStatus2 = bins2.find(bin => bin.predicate(randNum2)).status; //zoek eerste bin die true teruggeeft.
+  const parkingStatus3 = bins3.find(bin => bin.predicate(randNum3)).status; //zoek eerste bin die true teruggeeft.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{color: "red", margin: "20px"}}>Welkom bij Mosa Trajectum</h1>
+      <h4 style={{margin: "20px"}}>drukte bij de parking</h4>
+      <component>
+        <h4 className={parkingStatus}>OLV</h4>
+      </component>
+      <component>
+        <h4 className={parkingStatus2}>vrijhof</h4>
+      </component>
+      <component>
+        <h4 className={parkingStatus3}>colonel</h4>
+      </component>
+      
+      
     </div>
   );
 }
 
 export default App;
+
